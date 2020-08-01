d739b1149d4f42f15ce841718228a467b1dec1bd
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-01 15:31:26 +0300

    Added code to load information

--- src/phonebook/Application.java
+++ src/phonebook/Application.java
@@ -1,14 +1,20 @@
 package phonebook;
 
+import java.util.List;
+
 public class Application implements Runnable {
 
     private final PhoneBook phoneBook;
+    private final List<String> names;
 
-    public Application(PhoneBook phoneBook) {
+    public Application(final PhoneBook phoneBook, final List<String> names) {
         this.phoneBook = phoneBook;
+        this.names = names;
     }
 
     @Override
     public void run() {
+
+
     }
 }
--- src/phonebook/Main.java
+++ src/phonebook/Main.java
@@ -1,14 +1,22 @@
 package phonebook;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final String DIRECTORY_URL = "https://stepik.org/media/attachments/lesson/197761/directory.txt";
     private static final String FIND_URL = "https://stepik.org/media/attachments/lesson/197761/find.txt";
 
     public static void main(String[] args) throws IOException {
+
         new Application(
-                new PhoneBook(DIRECTORY_URL)
+                new PhoneBook(DIRECTORY_URL),
+                new Scanner(new URL(FIND_URL).openStream(), "UTF-8")
+                        .useDelimiter("\\R")
+                        .tokens()
+                        .collect(Collectors.toList())
         ).run();
     }
 }
