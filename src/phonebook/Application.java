a7d2a298adf2fffb4cd5f279dae972c43762818b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-01 16:15:42 +0300

    Added logging.properties file

--- src/phonebook/Application.java
+++ src/phonebook/Application.java
@@ -1,6 +1,7 @@
 package phonebook;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Application implements Runnable {
 
@@ -15,6 +16,12 @@ public class Application implements Runnable {
     @Override
     public void run() {
         System.out.println("Start searching...");
+        final var entries = names.size();
+        final var found = names.stream()
+                .map(phoneBook::findByNameLinearSearch)
+                .filter(Optional::isPresent)
+                .count();
 
+        System.out.printf("Found %d / %d entries. Time taken: 1 min. 56 sec. 328 ms.", found, entries);
     }
 }
