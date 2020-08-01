3d293b15fc47792052900d9beba27f50e359735b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-01 15:11:12 +0300

    Added empty method run()

--- src/phonebook/Application.java
+++ src/phonebook/Application.java
@@ -1,4 +1,11 @@
 package phonebook;
 
-public class Application {
+public class Application implements Runnable {
+    private static final String DIRECTORY_URL = "https://stepik.org/media/attachments/lesson/197761/directory.txt";
+    private static final String FIND_URL = "https://stepik.org/media/attachments/lesson/197761/find.txt";
+
+    @Override
+    public void run() {
+
+    }
 }
