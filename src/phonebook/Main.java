42f5e12e04ab0dba554c91b2b3f1ddc37b74e37c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-02 21:12:41 +0300

    Added static constants

--- src/phonebook/Main.java
+++ src/phonebook/Main.java
@@ -4,12 +4,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class Main {
     private static final Logger log = Logger.getLogger(Main.class.getName());
+    private static final Pattern LINE_DELIMITER = Pattern.compile("\\R");
+    private static final String DIRECTORY = "/home/jegors/Downloads/directory.txt";
+    private static final String NAMES = "/home/jegors/Downloads/find.txt";
 
     static {
         try {
@@ -21,12 +24,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         log.info("Program started.");
+
         new Application(
-                new PhoneBook(
-                        Files.readString(Path.of("/home/jegors/Downloads/directory.txt"))),
-                new Scanner(Files.readString(Path.of("/home/jegors/Downloads/find.txt")))
-                        .useDelimiter("\\R")
-                        .tokens()
+                new PhoneBook(Files.readString(Path.of(DIRECTORY))),
+                LINE_DELIMITER
+                        .splitAsStream(Files.readString(Path.of(NAMES)))
                         .toArray(String[]::new)
         ).run();
     }
