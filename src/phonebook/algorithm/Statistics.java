79f603fa1c0fdc1f1e95d380abf9cbedc2badd47
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-04 13:15:23 +0300

    Added Executor service

--- src/phonebook/algorithm/Statistics.java
+++ src/phonebook/algorithm/Statistics.java
@@ -2,14 +2,19 @@ package phonebook.algorithm;
 
 import phonebook.Record;
 
+import java.util.concurrent.*;
+
 import static java.lang.System.currentTimeMillis;
 
 public class Statistics {
+    private static long maxTime = 100_000;
+
     private final Record[] book;
     private final String[] names;
     private long sortTime;
     private long searchTime;
     private long recordsFound;
+    private boolean isSorted = false;
 
     public Statistics(Record[] phoneBook, String[] names) {
         this.book = phoneBook;
@@ -17,14 +22,35 @@ public class Statistics {
     }
 
     public Statistics performSort(SortAlgorithm algorithm) {
+        final var executor = Executors.newFixedThreadPool(4);
         final var start = currentTimeMillis();
-        algorithm.sort(book);
+
+        Future<?> future = executor.submit(() -> algorithm.sort(book));
+        executor.shutdown();
+
+        try {
+            future.get(maxTime, TimeUnit.MILLISECONDS);
+            isSorted = true;
+        } catch (InterruptedException e) {
+            System.out.println("job was interrupted");
+        } catch (ExecutionException e) {
+            System.out.println("caught exception: " + e.getCause());
+        } catch (TimeoutException e) {
+            future.cancel(true);
+            System.out.println("timeout");
+        }
+//        if(!executor.awaitTermination(2, TimeUnit.SECONDS)){
+            executor.shutdownNow();
+//        }
         sortTime = currentTimeMillis() - start;
         return this;
     }
 
     public Statistics performSearch(SearchAlgorithm algorithm) {
         final var start = currentTimeMillis();
+        if (!isSorted) {
+            algorithm = new LinearSearch();
+        }
         recordsFound = algorithm.getNamesFound(book, names);
         searchTime = currentTimeMillis() - start;
         return this;
@@ -37,9 +63,14 @@ public class Statistics {
             System.out.println("Sorting time: " + formatTime(sortTime));
             System.out.println("Searching time: " + formatTime(searchTime));
         }
+        System.out.println();
     }
 
     private String formatTime(final long timeMillis) {
         return String.format("%1$TM min. %1$TS sec. %1$TL ms.", timeMillis);
+    }
+
+    public static void setMaxTime(long time) {
+        maxTime = time;
     }
 }
